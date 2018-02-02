0a6f4455fa8c4130182314b41a477dbb9b7a5416
zhangming <zhangming.se.cn@gmail.com>
2018-02-02 09:46:32 +0800

    advice : after returning

--- src/aspect/AuthAspect.java
+++ src/aspect/AuthAspect.java
@@ -1,5 +1,6 @@
 package aspect;
 
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -9,5 +10,12 @@ public class AuthAspect {
 	@Before("execution(* aspect.*.*(..))")
 	public void authority() {
 		System.out.println("模拟执行权限检查");
+	}
+	
+	@AfterReturning(returning="rvt",
+			pointcut="execution(* aspect.*.*(..))")
+	public void log(Object rvt) {
+		System.out.println("获取目标方法的返回值" + rvt);
+		System.out.println("模拟记录日志的功能");
 	}
 }
