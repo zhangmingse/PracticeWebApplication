a2413f211e32f07b2841fb6e232e825187eaaba0
zhangming <zhangming.se.cn@gmail.com>
2018-02-02 09:52:29 +0800

    access the args of  target method.

--- src/aspect/AuthAspect.java
+++ src/aspect/AuthAspect.java
@@ -1,5 +1,6 @@
 package aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -8,8 +9,9 @@ import org.aspectj.lang.annotation.Before;
 public class AuthAspect {
 
 	@Before("execution(* aspect.*.*(..))")
-	public void authority() {
+	public void authority(JoinPoint jp) {
 		System.out.println("模拟执行权限检查");
+		System.out.println("before 增强处理 被织入增强处理的方法是：" + jp.getSignature().getName());
 	}
 	
 	@AfterReturning(returning="rvt",
